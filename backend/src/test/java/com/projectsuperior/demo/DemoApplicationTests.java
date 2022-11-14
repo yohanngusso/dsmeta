a4de3ba6facecae4690648ed921217ed9e31e707
Yohann Gusso <yohann.gusso@hotmail.com>
2022-11-14 19:52:19 -0300

    Project created

---
+++ backend/src/test/java/com/projectsuperior/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsuperior.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
