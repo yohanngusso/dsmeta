fca6239d2a41f9ab698f4c18268265aef4f323fc
Yohann Gusso <yohann.gusso@hotmail.com>
2022-11-26 20:12:22 -0300

    Twillio SMS

--- backend/src/main/java/com/projectsuperior/demo/controllers/SaleController.java
+++ backend/src/main/java/com/projectsuperior/demo/controllers/SaleController.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projectsuperior.demo.entities.Sale;
 import com.projectsuperior.demo.services.SaleService;
+import com.projectsuperior.demo.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -18,6 +20,9 @@ public class SaleController {
 	
 	@Autowired
 	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;  
 	
 	@GetMapping
 	//pageble para paginar em 20 elementos limitando 
@@ -28,4 +33,11 @@ public class SaleController {
 			Pageable pageable) {
 		return service.findSales(minDate, maxDate, pageable);
 	}
+	
+	@GetMapping("/{id}/notification")
+	
+	public void notify_sms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
+	
 }
